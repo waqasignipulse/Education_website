8bd4744d6a1f3ac1312b901e0a8fd39a9b1c4253
waqasignipulse <waqas.ignipulse@gmail.com>
2024-10-23 07:44:25 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'], // Corrected the quotation marks
+      urbanist: ['Urbanist', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
